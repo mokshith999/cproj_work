b18073a8f865eb2fb5ffec55107b508ef1b4e435
Mokshith69 <64922345+Mokshith69@users.noreply.github.com>
2025-12-20 15:17:34 +0100

    Change num from 6 to 5 in main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int main() {
     int x = 7, y = 12;
-    int num = 6;
+    int num = 5;
 
     printf("Biggest of %d and %d is %d\n", x, y, biggest(x, y));
     printf("Factorial of %d is %ld\n", num, factorial(num));
